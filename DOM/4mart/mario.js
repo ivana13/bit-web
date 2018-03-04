feb0ad48c9abfc553b161524085cbc422e5731e9
ivana13 <ivanaz133@gmail.com>
2018-03-04 21:50:45 +0100

    home

---
+++ DOM/4mart/mario.js
@@ -0,0 +1,35 @@
+var mario = document.querySelector(".stay");
+var marioRun = document.querySelector(".run");
+var bg = document.querySelector(".bg");
+bg.style.backgroundPositionX = '100px';
+var interval = "";
+var startRunBool = false;
+
+document.addEventListener("keydown", function(){
+   
+    
+    stay.style.display = "none";
+    run.style.display = "block";
+    
+    startRun();
+})
+
+document.addEventListener("keyup", function(){
+    run.style.display = "none";
+    stay.style.display = "block";
+    clearInterval(interval);
+    startRunBool = false;
+})
+
+function startRun() {
+    if(!startRunBool){
+        console.log("trcanje");
+        
+        interval = setInterval(groundMove, 50);
+        startRunBool = true;
+    }
+}
+
+function groundMove () {
+    bg.style.backgroundPositionX = (parseInt(bg.style.backgroundPositionX) - 10) + 'px';
+}
